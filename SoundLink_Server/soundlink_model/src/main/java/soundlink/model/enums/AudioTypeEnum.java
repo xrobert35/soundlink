3b154ade399d2bfac14fe6ea891db126aa4bc431
xrobert <asi04032013>
2017-04-11 01:40:21 +0200

    Ajout du WMA

--- SoundLink_Server/soundlink_model/src/main/java/soundlink/model/enums/AudioTypeEnum.java
+++ SoundLink_Server/soundlink_model/src/main/java/soundlink/model/enums/AudioTypeEnum.java
@@ -5,35 +5,35 @@ import java.util.List;
 
 public enum AudioTypeEnum {
 
-	FLAC(".flac"), MP3(".mp3"), WAVE(".wav"), M4A(".m4a"), OGG(".ogg");
+    FLAC(".flac"), MP3(".mp3"), WAVE(".wav"), M4A(".m4a"), OGG(".ogg"), WMA(".wma");
 
-	private String code;
+    private String code;
 
-	private AudioTypeEnum(String code) {
-		this.code = code;
-	}
+    private AudioTypeEnum(String code) {
+        this.code = code;
+    }
 
-	public String getCode() {
-		return code;
-	}
+    public String getCode() {
+        return code;
+    }
 
-	public static List<String> getAudioExtensions() {
-		List<String> extensions = new ArrayList<String>();
-		AudioTypeEnum[] values = AudioTypeEnum.values();
-		for (AudioTypeEnum audioType : values) {
-			extensions.add(audioType.getCode());
-		}
-		return extensions;
-	}
+    public static List<String> getAudioExtensions() {
+        List<String> extensions = new ArrayList<String>();
+        AudioTypeEnum[] values = AudioTypeEnum.values();
+        for (AudioTypeEnum audioType : values) {
+            extensions.add(audioType.getCode());
+        }
+        return extensions;
+    }
 
-	public static AudioTypeEnum getAudioTypeFromCode(String code) {
-		AudioTypeEnum[] values = AudioTypeEnum.values();
-		for (AudioTypeEnum audioType : values) {
-			if (audioType.getCode().equals(code)) {
-				return audioType;
-			}
-		}
+    public static AudioTypeEnum getAudioTypeFromCode(String code) {
+        AudioTypeEnum[] values = AudioTypeEnum.values();
+        for (AudioTypeEnum audioType : values) {
+            if (audioType.getCode().equals(code)) {
+                return audioType;
+            }
+        }
 
-		return null;
-	}
+        return null;
+    }
 }
